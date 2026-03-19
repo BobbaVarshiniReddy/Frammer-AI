793c10f5f8b73bf7aee54e6b6cd494435dde812d
AkshitaBhansali <akshitabhansali05@gmail.com>
2026-03-19 19:57:10 +0530

    deletingfiles

--- Agents/kpi_agents/query_engine.py
+++
@@ -1,9 +0,0 @@
-import duckdb
-
-def run_sql(df, query):
-    con = duckdb.connect()
-    con.register("df", df)
-
-    result = con.execute(query).fetchdf()
-
-    return result.to_dict(orient="records")
